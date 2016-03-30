dc26b816148db238339d187ec3eeedac13ba8931
Charles Coulton <Charles@Charless-MacBook-Air.local>
2016-03-30 15:17:26 -0700

    start of 3.0 updates

--- src/main/java/com/qwasi/sdk/QwasiGCMDefault.java
+++ src/main/java/com/qwasi/sdk/QwasiGCMDefault.java
@@ -1,5 +1,7 @@
 package com.qwasi.sdk;
 
+import android.os.Bundle;
+
 /**
  * Created by ccoulton on 1/15/16.
  * Provides a concrete class to communicate w/ QwasiGCMListener
@@ -7,4 +9,7 @@ package com.qwasi.sdk;
 public class QwasiGCMDefault extends QwasiGCMListener{
     @Override
     public void onQwasiMessage(QwasiMessage msg){}
+
+    @Override
+    public void onQwasiBundle(Bundle msg){}
 }
